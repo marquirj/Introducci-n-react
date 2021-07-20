31cb00541169543bdd546227d2c72571bf2d68fe
marquirj <udt.popes@gmail.com>
2021-07-20 20:27:04 +0200

    ejemplo asyn

---
+++ 02-intro-javascript/src/bases/11-async.js
@@ -0,0 +1,16 @@
+const getImagen = async() => {
+    try{
+        const apiKey = 'cgrHKIfTYvbuQ1WHzP9V5BEX31usAgss';
+        const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const {data} = await resp.json();
+        const {url} = data.images.original;
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+    }catch(error){
+        console.error(error);
+    }
+    
+}
+    
+getImagen();
