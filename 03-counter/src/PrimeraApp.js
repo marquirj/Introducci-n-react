1aaa34814dd3f3a7968c6c6d56f98b3ab5895daf
marquirj <udt.popes@gmail.com>
2021-07-27 19:39:44 +0200

    modificar para pruebas

--- 03-counter/src/PrimeraApp.js
+++ 03-counter/src/PrimeraApp.js
@@ -1,11 +1,18 @@
 import React from 'react';
-const PrimeraApp = () => {
+import PropTypes from 'prop-types';
+const PrimeraApp = ({saludo, subtitulo}) => {
     return  (
         <>
-            <h1>Hola Mundo</h1>
-            <p>Mi primera aplicación</p>
+            <h1>{saludo}</h1>
+            <p>{subtitulo}</p>
         </>
     );
 
+}
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
 }
 export default PrimeraApp;
