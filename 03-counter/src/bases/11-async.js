292dd9da6e621fef84c447a032ac43856c2fb83a
marquirj <udt.popes@gmail.com>
2021-07-27 17:24:43 +0200

    modificamos consolelog

--- 03-counter/src/bases/11-async.js
+++ 03-counter/src/bases/11-async.js
@@ -1,16 +1,17 @@
-const getImagen = async() => {
+export const getImagen = async() => {
     try{
         const apiKey = 'cgrHKIfTYvbuQ1WHzP9V5BEX31usAgss';
         const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
         const {data} = await resp.json();
         const {url} = data.images.original;
-        const img = document.createElement('img');
+        return url
+        /*const img = document.createElement('img');
         img.src = url;
-        document.body.append(img);
+        document.body.append(img);*/
     }catch(error){
-        console.error(error);
+        //console.error(error);
+        return 'No existe';
     }
     
 }
     
-getImagen();
